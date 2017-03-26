4bfa11edfabf1b8b95afbf8659e73a0315a69c3b
Lukas <seriouz.accz@gmail.com>
2017-03-26 22:23:50 +0200

    Add stub of Zomato menu provider

---
+++ src/main/java/pro/absolutne/lunchagator/lunch/provider/ZomatoMenuProvider.java
@@ -0,0 +1,11 @@
+package pro.absolutne.lunchagator.lunch.provider;
+
+
+public class ZomatoMenuProvider {
+
+    private static final String URL = "https://developers.zomato.com/api/v2.1/dailymenu?res_id=16507679";
+
+    public void get() {
+
+    }
+}
