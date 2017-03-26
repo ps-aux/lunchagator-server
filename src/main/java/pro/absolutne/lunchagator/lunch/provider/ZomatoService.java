130c18b5d38bfefcde31e8e10f9a6c0b4b134e71
Lukas <seriouz.accz@gmail.com>
2017-03-26 22:24:23 +0200

    Rename  to ZomatoService

--- src/main/java/pro/absolutne/lunchagator/lunch/provider/ZomatoService.java
+++ src/main/java/pro/absolutne/lunchagator/lunch/provider/ZomatoService.java
@@ -1,7 +1,7 @@
 package pro.absolutne.lunchagator.lunch.provider;
 
 
-public class ZomatoMenuProvider {
+public class ZomatoService {
 
     private static final String URL = "https://developers.zomato.com/api/v2.1/dailymenu?res_id=16507679";
 
