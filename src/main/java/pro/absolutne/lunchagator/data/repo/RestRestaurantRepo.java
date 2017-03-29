9ae949be5a74d72e0f493ba56e71723abe0b0258
Lukas <seriouz.accz@gmail.com>
2017-03-29 23:23:51 +0200

    Set cors on restaurant repo

--- src/main/java/pro/absolutne/lunchagator/data/repo/RestRestaurantRepo.java
+++ src/main/java/pro/absolutne/lunchagator/data/repo/RestRestaurantRepo.java
@@ -2,8 +2,10 @@ package pro.absolutne.lunchagator.data.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pro.absolutne.lunchagator.data.entity.Restaurant;
 
+@CrossOrigin
 @RepositoryRestResource
 public interface RestRestaurantRepo extends CrudRepository<Restaurant, Long> {
 
