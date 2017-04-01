e6b9c1e9e676955148f1498129697c5d2e1ad79c
Lukas <seriouz.accz@gmail.com>
2017-04-01 09:29:53 +0200

    Add Dockerfile & Circle ci 2.0 config

--- src/test/java/pro/absolutne/LunchagatorApplicationTests.java
+++ src/test/java/pro/absolutne/LunchagatorApplicationTests.java
@@ -1,12 +1,16 @@
 package pro.absolutne;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import pro.absolutne.lunchagator.LunchagatorApplication;
 
+// TODO setup db
+@Ignore
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = LunchagatorApplication.class)
 public class LunchagatorApplicationTests {
 
 	@Test
