300cdb37f00cc1d159e11eb96fa7e63de54f5e57
ps-aux <seriouz.accz@gmail.com>
2017-04-03 13:59:08 +0200

    Add entrypoint.sh do Docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+check_var() {
+   if [[ -z "${!1}" ]]; then
+        echo "Missing env variable ${1}" >&2
+        exit 1
+   fi
+}
+
+check_var DB_JDBC_URL
+check_var DB_USER_NAME
+check_var DB_USER_PASS
+
+export SPRING_DATASOURCE_URL="$DB_JDBC_URL"
+export SPRING_DATASOURCE_PASSWORD="$DB_USER_NAME"
+export SPRING_DATASOURCE_USERNAME="$DB_USER_PASS"
+
+java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar
