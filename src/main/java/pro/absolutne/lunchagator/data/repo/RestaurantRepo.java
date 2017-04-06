d3e61ad4027620590eda30951a092b3785778008
Lukas <seriouz.accz@gmail.com>
2017-04-06 21:15:48 +0200

    Fix rest repo problem with two restaurant repos

--- src/main/java/pro/absolutne/lunchagator/data/repo/RestaurantRepo.java
+++ src/main/java/pro/absolutne/lunchagator/data/repo/RestaurantRepo.java
@@ -3,11 +3,16 @@ package pro.absolutne.lunchagator.data.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pro.absolutne.lunchagator.data.entity.Restaurant;
 
 import java.util.Optional;
 
-public interface RestaurantRepo extends JpaRepository<Restaurant, String> {
+
+@CrossOrigin
+@RepositoryRestResource
+public interface RestaurantRepo extends JpaRepository<Restaurant, Long> {
 
     @Query("SELECT r from Restaurant r where r.menuProviderInfo.zomatoId = :id")
     Optional<Restaurant> findByZomatoId(@Param("id") int id);
